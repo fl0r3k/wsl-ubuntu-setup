6fd4e249ce0e5f92936c13a583348c8d12be57d6
Piotr Florczyk <piotr.florczyk@aller.com>
2025-03-20 13:15:17 +0100

    some nvim plugins adjustments

--- dotfiles/.config/nvim/lua/plugins/obsidian.lua
+++ dotfiles/.config/nvim/lua/plugins/obsidian.lua
@@ -23,5 +23,29 @@ return {
       time_format = "%H:%M",
       tags = "",
     },
+    --completion = {
+    --  nvim_cmp = true,
+    --  min_chars = 2,
+    --  new_notes_location = "current_dir",
+    --  prepend_note_id = true,
+    --},
+    note_frontmatter_func = function(note)
+      -- Add the title of the note as an alias.
+      if note.title then
+        note:add_alias(note.title)
+      end
+
+      local out = { id = note.id, aliases = note.aliases, tags = note.tags, area = "", project = "" }
+
+      -- `note.metadata` contains any manually added fields in the frontmatter.
+      -- So here we just make sure those fields are kept in the frontmatter.
+      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+        for k, v in pairs(note.metadata) do
+          out[k] = v
+        end
+      end
+
+      return out
+    end,
   },
 }
